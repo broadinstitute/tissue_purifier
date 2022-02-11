c146b5c761060a142853698df6293a39d73b8841
dalessioluca <ldalessi@broadinstitute.org>
2022-02-11 16:35:28 -0500

    make null distribution

--- src/tissue_purifier/misc_utils/validation.py
+++ src/tissue_purifier/misc_utils/validation.py
@@ -5,13 +5,12 @@ from typing import Callable
 def create_null_distribution(
         cell_types_n: torch.Tensor,
         counts_ng: torch.Tensor,
-        fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]):
+        similarity_measure: str = "L1",):
     """
     Args:
         cell_types_n: 1D tensor or array with the cell_type information
         counts_ng: 2D array with the counts for each cell n and gene g
-        fn: any function which compare the gene expression of two random cells (of the same types)
-            and return a measure of similarity
+        similarity_measure: type of statistic to compute between gene expressions. If defaults to the L1
 
     Returns:
          dict where the keys are the cell_types and the values are a list of the computed statistics for all
@@ -22,15 +21,26 @@ def create_null_distribution(
     unique_cell_types = torch.unique(cell_types_n)
     for k, ctype in enumerate(unique_cell_types):
         counts_kg = counts_ng[cell_types_n == ctype]
+
         n_max = counts_kg.shape[0]
         size = int(0.5 * n_max * (n_max + 1))
-        n = 0
+
         for i1 in range(n_max):
-            for i2 in range(i1+1, n_max):
-                sim = fn(counts_kg[i1, :], counts_kg[i2, :])
-                if i1 == 0 and i2 == 1:
-                    similarity = torch.zeros(size, sim.shape[-1])
-                similarity[n, :] = sim
-                n += 1
+            mask = (torch.arange(n_max) == i1)
+            gene_exp_ref = counts_kg[mask, :]
+            gene_exp_others = counts_kg[~mask, :]
+            if similarity_measure == "L1":
+                sim = (gene_exp_ref - gene_exp_others).abs().cpu().numpy()
+            else:
+                raise ValueError("similarity_measure not valid. Received {0}".format(similarity_measure))
+
+            if i1 == 0:
+                similarity = torch.zeros(size, sim.shape[-1]).cpu().numpy()
+
+            i_start = i1*(n_max-1)
+            i_end = (i1+1)*(n_max-1)
+            print(i_start, i_end)
+            similarity[i_start:i_end, :] = sim
+
         result_dict["cell_type_"+str(k)] = similarity
     return result_dict
