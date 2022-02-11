225e1628ab1c6d42181d16ccc66424fe52b2edb1
dalessioluca <ldalessi@broadinstitute.org>
2022-02-11 15:58:47 -0500

    make null distribution

---
+++ src/tissue_purifier/misc_utils/validation.py
@@ -0,0 +1,36 @@
+import torch
+import numpy
+from typing import Any, Callable, Union
+
+def create_null_distribution(
+        cell_types_n: torch.Tensor,
+        counts_ng: torch.Tensor,
+        fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]):
+    """
+    Args:
+        cell_types_n: 1D tensor or array with the cell_type information
+        counts_ng: 2D array with the counts for each cell n and gene g
+        fn: any function which compare the gene expression of two random cells (of the same types)
+            and return a measure of similarity
+
+    Returns:
+         dict where the keys are the cell_types and the values are a list of the computed statistics for all
+         possible pairs of cells
+    """
+
+    result_dict = dict()
+    unique_cell_types = torch.unique(cell_types_n)
+    for k, ctype in enumerate(unique_cell_types):
+        counts_kg = counts_ng[cell_types_n == ctype]
+        n_max = counts_kg.shape[0]
+        size = int(0.5 * n_max * (n_max + 1))
+        n = 0
+        for i1 in range(n_max):
+            for i2 in range(i1+1, n_max):
+                sim = fn(counts_kg[i1, :], counts_kg[i2, :])
+                if i1 == 0 and i2 == 1:
+                    similarity = torch.zeros(size, sim.shape[-1])
+                similarity[n, :] = sim
+                n += 1
+        result_dict[ctype] = similarity
+    return result_dict
