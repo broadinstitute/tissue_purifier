89d579a8d0c7b0b944d036af399c08f6e71a5afa
dalessioluca <ldalessi@broadinstitute.org>
2022-02-12 17:22:12 -0500

    p-values

--- src/tissue_purifier/misc_utils/validation.py
+++ src/tissue_purifier/misc_utils/validation.py
@@ -1,67 +1,116 @@
 import torch
 
 
+def _make_binning(_x_ng, _boundaries):
+    """ Bins the metric for each cell and gene given the boundaries tensor. """
+    b = _boundaries.shape[0]  # shape: (bins)
+    n, g = _x_ng.shape[:2]   # shape: (n_sample, n_genes)
+
+    _index_ng = torch.bucketize(_x_ng, _boundaries)
+    _src = torch.ones((n, g, b))
+    _counter = torch.zeros_like(_src).scatter(dim=-1, index=_index_ng.unsqueeze(dim=-1), src=_src)
+    return _counter.sum(dim=0)  # sum over sample. shape: (genes, bins)
+
+
 def create_null_distribution(
         cell_types_n: torch.Tensor,
         counts_ng: torch.Tensor,
         similarity_measure: str = "L1",
-        boundaries: torch.Tensor = torch.linspace(start=0, end=100, steps=101),
-        interval_close_to_the_right: bool = False) -> dict:
+        boundaries: torch.Tensor = torch.linspace(start=0, end=30, steps=31)) -> dict:
     """
-    Create the null distribution by comparing all possible pair of cells of the same type.
-    This can be very computationally end memory expensive.
-    Consider passing in a subset of cell_types and/or genes.
+    Compute the similarity between all possible pairs of cells cells of the same type.
+    The similarity is binned and the resulting histogram is returned.
 
     Args:
         cell_types_n: 1D tensor or array with the cell_type information
         counts_ng: 2D array with the counts for each cell n and gene g
         similarity_measure: type of statistic to compute between gene expressions. If defaults to the L1
-        boundaries: the statistics will be binned in these intervals. Must be in increasing order.
-        interval_close_to_the_right: If False (Defaults) returns index i
-            which satisfies boundaries[i-1] < x <= boundaries[i]
-            If True, returns i such that boundaries[i-1] <= x < boundaries[i]
-
-    Note:
-        Values are clipped to edges, i.e. if x > boundaries[-1] it return the last index.
-
-
+        boundaries: this specify how to bin the data
 
     Returns:
          null_dict. Dictionary with distribution of the statistis under the null model.
             The dictionary includes the bins used and the bin_counter for each gene.
     """
 
-    def _make_binning(_x_ng, _bins):
-        mask_bng = (_x_ng < _bins[:, None, None])
-        # find the last true
-        index_bng =
-        _x_ng <
 
-    result_dict = {"bins": bins}
+    if torch.cuda.is_available():
+        cell_types_n = cell_types_n.cuda()
+        counts_ng = counts_ng.cuda()
+        boundaries = boundaries.cuda()
+
+    result_dict = {"boundaries": boundaries}
     unique_cell_types = torch.unique(cell_types_n)
     g = counts_ng.shape[-1]
-    n_bins = bins.shape[0]
 
     for k, ctype in enumerate(unique_cell_types):
         counts_kg = counts_ng[cell_types_n == ctype]
 
-        bin_counter = torch.zeros(g, n_bins).cpu().numpy()
+        bin_counter_gb = torch.zeros(g, boundaries.shape[0]).cpu().numpy()
         for i1 in range(1, counts_kg.shape[0]):
-            gene_exp_others = counts_kg[:i1, :]
-            gene_exp_ref = counts_kg[i1, :]
+            gene_exp_others_mg = counts_kg[:i1, :]
+            gene_exp_ref_1g = counts_kg[i1, :]
 
             if similarity_measure == "L1":
-                sim = (gene_exp_ref - gene_exp_others).abs()
+                sim_mg = (gene_exp_ref_1g - gene_exp_others_mg).abs()
             else:
                 raise ValueError("similarity_measure not valid. Received {0}".format(similarity_measure))
 
-            index_ng = torch.bucketize(sim, boundaries, right=interval_close_to_the_right)
-            torch.bincount()
+            bin_increment_gb = _make_binning(sim_mg, boundaries)
+            bin_counter_gb += bin_increment_gb.cpu().numpy()
+
+        result_dict["cell_type_"+str(k)] = bin_counter_gb
+    return result_dict
 
 
-            assert bin_increment.shape == torch.Size([g, n_bins])
-            bin_counter += bin_increment.cpu().numpy()
+def create_heldout_distribution(
+        cell_types_n: torch.Tensor,
+        true_counts_ng: torch.Tensor,
+        pred_counts_ng: torch.Tensor,
+        similarity_measure: str = "L1",
+        boundaries: torch.Tensor = torch.linspace(start=0, end=30, steps=31)) -> dict:
+    """
+    Compute the similarity between all possible pairs of cells cells of the same type.
+    The similarity is binned and the resulting histogram is returned.
 
-        result_dict["cell_type_"+str(k)] = bin_counter
-    result_dict["bins"] = bins
+    Args:
+        cell_types_n: 1D tensor or array with the cell_type information
+        true_counts_ng: 2D array with the ture counts for each cell n and gene g
+        pred_counts_ng: 2D array with the predicted counts for each cell n and gene g
+        similarity_measure: type of statistic to compute between gene expressions. If defaults to the L1
+        boundaries: this specify how to bin the data
+
+    Returns:
+         heldout_dict. Dictionary with distribution of the statistics for the heldout data.
+            The dictionary includes the bins used and the bin_counter for each gene.
+    """
+
+    if torch.cuda.is_available():
+        cell_types_n = cell_types_n.cuda()
+        true_counts_ng = true_counts_ng.cuda()
+        pred_counts_ng = pred_counts_ng.cuda()
+        boundaries = boundaries.cuda()
+
+    assert true_counts_ng.shape == pred_counts_ng.shape
+    assert cell_types_n.shape[0] == true_counts_ng.shape[0]
+
+    result_dict = {"boundaries": boundaries}
+    unique_cell_types = torch.unique(cell_types_n)
+    g = true_counts_ng.shape[-1]
+
+    for k, ctype in enumerate(unique_cell_types):
+        mask_k_type = (cell_types_n == ctype)
+        true_counts_kg = true_counts_ng[mask_k_type]
+        pred_counts_kg = pred_counts_ng[mask_k_type]
+
+        bin_counter_gb = torch.zeros(g, boundaries.shape[0]).cpu().numpy()
+
+        if similarity_measure == "L1":
+            sim_mg = (true_counts_kg - pred_counts_kg).abs()
+        else:
+            raise ValueError("similarity_measure not valid. Received {0}".format(similarity_measure))
+
+        bin_increment_gb = _make_binning(sim_mg, boundaries)
+        bin_counter_gb += bin_increment_gb.cpu().numpy()
+
+        result_dict["cell_type_"+str(k)] = bin_counter_gb
     return result_dict
