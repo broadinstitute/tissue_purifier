d73a48a041db28cc10b604e54762d29d70ae253c
dalessioluca <ldalessi@broadinstitute.org>
2022-02-28 16:15:35 -0500

    source

--- src/tissue_purifier/__init__.py
+++ src/tissue_purifier/__init__.py
@@ -1,3 +1,13 @@
-# flake8: noqa
-# fmt: off
-# fmt: on
+__version__ = "0.0.1"
+
+from tissue_purifier.model_utils.barlow import BarlowModel
+from tissue_purifier.model_utils.vae import VaeModel
+from tissue_purifier.model_utils.dino import DinoModel
+from tissue_purifier.model_utils.simclr import SimclrModel
+
+__all__ = [
+        "BarlowModel",
+        "VaeModel",
+        "DinoModel",
+        "SimclrModel",
+]
