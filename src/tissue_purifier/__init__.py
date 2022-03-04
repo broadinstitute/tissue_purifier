309192cf11688776eb6d40b67d16d7e21a090d0c
dalessioluca <ldalessi@broadinstitute.org>
2022-03-04 09:42:02 -0500

    init_files

--- src/tissue_purifier/__init__.py
+++ src/tissue_purifier/__init__.py
@@ -5,9 +5,12 @@ from tissue_purifier.model_utils.ssl_models.vae import VaeModel
 from tissue_purifier.model_utils.ssl_models.dino import DinoModel
 from tissue_purifier.model_utils.ssl_models.simclr import SimclrModel
 
+from tissue_purifier.io_utils.download import download_from_bucket
+
 __all__ = [
         "BarlowModel",
         "VaeModel",
         "DinoModel",
         "SimclrModel",
+        "download_from_bucket"
 ]
---
+++ src/tissue_purifier/test.py
@@ -0,0 +1,9 @@
+import tissue_purifier 
+import tissue_purifier.download_from_bucket
+
+print("WOW 1!")
+
+import tissue_purifier as tp
+import tp.download_from_bucket
+
+print("WOW 2!")
