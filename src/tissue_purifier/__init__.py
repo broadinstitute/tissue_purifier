5e70cabf1b3d3832e94905ae775be996cafb6440
dalessioluca <ldalessi@broadinstitute.org>
2022-03-07 11:12:36 -0500

    run models

--- src/tissue_purifier/__init__.py
+++ src/tissue_purifier/__init__.py
@@ -1,24 +1,11 @@
 __version__ = "0.0.1"
 
 # The actual API
-from tissue_purifier import data_utils as data
-from tissue_purifier import gene_regression as genex
-from tissue_purifier import io_utils as io
-from tissue_purifier import model_utils as models
-from tissue_purifier import plot_utils as plot
-from tissue_purifier import misc_utils as utils
-
-
 __all__ = [
     "data",
     "genex",
     "utils",
     "io",
     "models",
-    "plot",
+    "plots",
 ]
-
-# it means that you can do
-# import tissue_purifier as tp
-# a = tp.data.AnndatFolderDM
-
--- src/tissue_purifier/test.py
+++
@@ -1,9 +0,0 @@
-import tissue_purifier 
-import tissue_purifier.download_from_bucket
-
-print("WOW 1!")
-
-import tissue_purifier as tp
-import tp.download_from_bucket
-
-print("WOW 2!")
