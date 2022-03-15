1ef59aa24519948b5a5a97f635a4355bdbd033af
dalessioluca <ldalessi@broadinstitute.org>
2022-03-15 00:37:18 -0400

    pretrained

--- src/tissue_purifier/genex/gene_utils.py
+++ src/tissue_purifier/genex/gene_utils.py
@@ -109,8 +109,7 @@ def make_gene_dataset_from_anndata(
 
     if not torch.all(torch.isfinite(covariates_nl_raw)):
         mask = torch.isfinite(covariates_nl_raw)
-        print("there are n={} not finite covariates".format(torch.sum(~mask))
-        raise ValueError("covariates in the anndata file are not finite.")
+        raise ValueError("there are n={} not finite covariates in anndata file".format(torch.sum(~mask)))
 
     assert len(counts_ng.shape) == 2
     assert len(covariates_nl_raw.shape) == 2
