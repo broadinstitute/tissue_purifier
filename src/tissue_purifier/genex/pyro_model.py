ccb2289bb7bd5a02104a8805c7ecf0806f745194
dalessioluca <ldalessi@broadinstitute.org>
2022-03-15 00:39:19 -0400

    pretrained

--- src/tissue_purifier/genex/pyro_model.py
+++ src/tissue_purifier/genex/pyro_model.py
@@ -239,10 +239,11 @@ class GeneRegression:
                 if use_covariates:
                     log_mu_n1g = beta0_n1g[..., ind_g] + \
                                  torch.sum(covariate_sub_nl1 * beta_nlg[..., ind_g], dim=-2, keepdim=True)
-                    print("cazzo 1", log_mu_n1g)
-                    print("cazzo 2", beta0_n1g[..., ind_g])
-                    print("cazzo 3", covariate_sub_nl1)
-                    print("cazzo 4", beta_nlg[..., ind_g])
+                    print("cazzo 1", log_mu_n1g.shape)
+                    print("cazzo 2", beta0_n1g[..., ind_g].shape)
+                    print("cazzo 3", covariate_sub_nl1.shape)
+                    print("cazzo 4", beta_nlg[..., ind_g].shape)
+                    assert 1==2
                 else:
                     log_mu_n1g = beta0_n1g[..., ind_g]
 
