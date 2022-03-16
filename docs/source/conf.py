9d3b30e2e4e6e2b150b52907b834411f3d24e442
dalessioluca <ldalessi@broadinstitute.org>
2022-03-16 02:01:29 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,8 +117,8 @@ add_module_names = False
 nbsphinx_prolog = ""
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = "alabaster" #"sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"  # "alabaster" #"sphinx_rtd_theme"
+html_static_path = [ ]  # "_static"]
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ["_static"]
 #html_style = "css/tissuepurifier.css"
 #htmlhelp_basename = "tissuepurifierdoc"
